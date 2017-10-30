5904c3cb2bfed4438cedf89817dbc9519dbc15ea
Harold Hatch <harold.hatch@nist.gov>
2017-10-30 14:22:35 -0400

    Removed TrialMD and test directory.

--- test/ljLambdaBinarySemiGrand/cpp/run.sh
+++
@@ -1,6 +0,0 @@
-prog=semigrand
-FEASST_INSTALL_DIR_=$HOME/jeetain/feasstv0.1.3/
-echo $FEASST_INSTALL_DIR_
-$FEASST_INSTALL_DIR_/tools/compile.sh $prog
-mkdir -p tmp #directory for checkpoint files
-./$prog
