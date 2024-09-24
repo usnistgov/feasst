8d3f133fb3f2dd6562510a70dfdea7eb6501a39e
Daniel W. Siderius <daniel.siderius@nist.gov>
2024-09-24 15:20:31 -0400

    updating HenryCoefficient to save Taylor-extrapolation coefficients  and terms for covariance of Taylor-extrapolation

--- plugin/confinement/src/henry_coefficient.cpp
+++ plugin/confinement/src/henry_coefficient.cpp
@@ -26,8 +26,13 @@ HenryCoefficient::HenryCoefficient(argtype * args) : Analyze(args) {
   ASSERT(trials_per_update() == 1, "should update every step");
   const int num_beta_taylor = integer("num_beta_taylor", args, 0);
   beta_taylor_.resize(num_beta_taylor);
+  beta_taylor2_.resize(2*num_beta_taylor);
   for (int ibt = 0; ibt < num_beta_taylor; ++ibt) {
     beta_taylor_[ibt] = *MakeAccumulator({{"num_moments", "2"},
+                                          {"max_block_operations", "0"}});
+  }
+  for (int ibt = 0; ibt < 2*num_beta_taylor; ++ibt) {
+    beta_taylor2_[ibt] = *MakeAccumulator({{"num_moments", "2"},
                                           {"max_block_operations", "0"}});
   }
   write_precision_ = dble("write_precision", args, 8);
@@ -75,14 +80,23 @@ void HenryCoefficient::update(const Criteria& criteria,
   get_accumulator()->accumulate(std::exp(-beta*delta_en));
   DEBUG("acc " << accumulator().str());
 
-  // moments for extrapolation
+  // moments for extrapolation including covariance terms
   double unebu = std::exp(-beta*delta_en);
+  double unebu2 = std::exp(-2.0*beta*delta_en);
   DEBUG("unebu " << unebu);
+  DEBUG("unebu2 " << unebu2);
   for (int ibd = 0; ibd < static_cast<int>(beta_taylor_.size()); ++ibd) {
     unebu *= -delta_en;
     DEBUG("ibd " << ibd << " unebu " << unebu);
     beta_taylor_[ibd].accumulate(unebu/factorial(ibd+1));
+    unebu2 *= -delta_en;
+    DEBUG("2*ibd " << 2*ibd << " unebu2 " << unebu2);
+    beta_taylor2_[2*ibd].accumulate(unebu2/factorial(2*ibd+1)/factorial(2*ibd+1));
+    unebu2 *= -delta_en;
+    DEBUG("2*ibd+1 " << 2*ibd+1 << " unebu2 " << unebu2);
+    beta_taylor2_[2*ibd+1].accumulate(unebu2/factorial(2*ibd+2)/factorial(2*ibd+2));
   }
+
 }
 
 std::string HenryCoefficient::write(const Criteria& criteria,
@@ -90,11 +104,22 @@ std::string HenryCoefficient::write(const Criteria& criteria,
     const TrialFactory& trial_factory) {
   std::stringstream ss;
   if (num_beta_taylor() > 0) {
-    ss << "#{\"beta_taylor\": [";
+    ss << "#{";
+    ss << "\"beta\":" << std::setprecision(write_precision_) << system.thermo_params().beta() << ",";
+    ss << "\"num_trials\":" << accumulator().moment(0) << ",";
+    ss << "\"beta_taylor\": [";
     ss << std::setprecision(write_precision_) << accumulator().average() << ",";
     for (int ibt = 1; ibt < num_beta_taylor() + 1; ++ibt) {
       ss << std::setprecision(write_precision_)
-         << beta_taylor_[ibt - 1].average() << ",";
+         << beta_taylor_[ibt - 1].average();
+      if (ibt < num_beta_taylor()) ss << ",";
+    }
+    ss << "], \"beta_taylor2\": [";
+    ss << std::setprecision(write_precision_) << accumulator().moment(2)/accumulator().moment(0) << ",";
+    for (int ibt = 1; ibt < 2*num_beta_taylor() + 1; ++ibt) {
+      ss << std::setprecision(write_precision_)
+         << beta_taylor2_[ibt - 1].average();
+      if (ibt < 2*num_beta_taylor()) ss << ",";
     }
     ss << "]}";
   }
@@ -111,6 +136,7 @@ void HenryCoefficient::serialize(std::ostream& ostr) const {
   Stepper::serialize(ostr);
   feasst_serialize_version(9494, ostr);
   feasst_serialize_fstobj(beta_taylor_, ostr);
+  feasst_serialize_fstobj(beta_taylor2_, ostr);
   feasst_serialize(write_precision_, ostr);
 }
 
@@ -121,6 +147,7 @@ HenryCoefficient::HenryCoefficient(std::istream& istr) : Analyze(istr) {
     feasst_deserialize_fstobj(&beta_taylor_, istr);
   }
   if (version >= 9494) {
+    feasst_deserialize_fstobj(&beta_taylor2_, istr);
     feasst_deserialize(&write_precision_, istr);
   }
 }
